6aa993a91386374e12ade00fb9bf16d996cade96
xiong6456 <hevinxiong@163.com>
2018-07-19 21:49:24 +0800

    生成验证码工具类

--- src/main/java/com/zero/controller/shiro/LoginController.java
+++ src/main/java/com/zero/controller/shiro/LoginController.java
@@ -1,7 +1,8 @@
 package com.zero.controller.shiro;
 
-import javax.servlet.http.HttpServletRequest;
-
+import com.zero.pojo.SysUser;
+import com.zero.service.shiro.SysUserService;
+import com.zero.util.ValidateCode;
 import net.sf.json.JSONObject;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
@@ -13,13 +14,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.zero.pojo.SysUser;
-import com.zero.service.shiro.SysUserService;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import org.apache.shiro.web.util.WebUtils;
+import org.apache.commons.lang.StringUtils;
 /**
  * @author Hevin*Xiong
  * @version 1.0.0
@@ -36,8 +42,18 @@ public class LoginController {
 
     @ResponseBody
     @RequestMapping("/login")
-    public String login(SysUser user, Model model, HttpServletRequest request) {
+    public String login(HttpServletRequest request,HttpSession session) {
         JSONObject jsonObject = new JSONObject();
+
+        //页面生成的验证码
+        String code = (String) session.getAttribute("validateCode");
+        //输入的验证码
+        String submitCode = WebUtils.getCleanParam(request, "validateCode");
+        if (StringUtils.isEmpty(submitCode) || !StringUtils.equals(code,submitCode.toLowerCase())) {
+            jsonObject.put("loginRes", "验证码错误！");
+            return jsonObject.toString();
+        }
+
         String fdLoginName = request.getParameter("fdLoginName");
         String fdPassword = request.getParameter("fdPassword");
         Subject subject = SecurityUtils.getSubject();
@@ -46,8 +62,7 @@ public class LoginController {
         try {
             subject.login(usernamePasswordToken);
             logger.info("======登陆成功=======");
-            SysUser userInfo = userService.queryUserByName(user
-                    .getFdLoginName());
+            SysUser userInfo = userService.queryUserByName(fdLoginName);
             subject.getSession().setAttribute("userInfo", userInfo);
             // 查询菜单
 
@@ -72,9 +87,25 @@ public class LoginController {
     }
 
     @RequestMapping("/exit")
-    public String login(HttpServletRequest request) {
+    public String exit(HttpServletRequest request) {
         Subject subject = SecurityUtils.getSubject();
         subject.logout();
         return "redirect:/login.jsp";
+    }
+
+    /**
+     * 生成验证码
+     * @param request
+     * @param response
+     * @throws IOException
+     */
+    @RequestMapping("/validateCode")
+    public void validateCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setHeader("Cache-Control", "no-cache");
+        String verifyCode = ValidateCode.generateTextCode(ValidateCode.TYPE_NUM_ONLY, 4, null);
+        request.getSession().setAttribute("validateCode", verifyCode);
+        response.setContentType("image/jpeg");
+        BufferedImage bim = ValidateCode.generateImageCode(verifyCode, 90, 30, 3, true, Color.WHITE, Color.BLACK, null);
+        ImageIO.write(bim, "JPEG", response.getOutputStream());
     }
 }
